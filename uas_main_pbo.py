d135c029b6156bd7a1a76d8bac8ecbf532e5b1cb
andryrondo123 <andryrondo123@gmail.com>
2025-01-17 22:58:27 +0700

    Update uas_main_pbo.py

--- uas_main_pbo.py
+++ uas_main_pbo.py
@@ -142,6 +142,9 @@ elif npm == "073":
 elif npm == "087":
     # silahkan isi 2 (dua) baris Maksimal untuk memanggil fungsinya samsulidan
     samsulidan()
+elif npm == "096":
+    # silahkan isi 2 (dua) baris Maksimal untuk memanggil fungsinya Andry Sevcenko
+    andry()
 # =======
 # >>>>>>> main
 # >>>>>>> main
